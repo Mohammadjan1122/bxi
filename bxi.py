e776669773db72f82298f53334b8c2c0a2dc378b
Binyamin Binni <53377458+Binyamin-binni@users.noreply.github.com>
2020-11-22 15:11:08 +0500

    Create bxi.py

---
+++ bxi.py
@@ -0,0 +1,2 @@
+print 3*("\n")
+print(" This tool is not available right now\n we are working it\n to grt update please subscribe our\n youtube channel TRICKPROOF\n click on the following link\n https://www.youtube.com/channel/UC5kptDqLxcVCzHgayaBuBPA")
