b3d3951528e9e5eb80e97f5dd30939d0536b5b23
Binyamin Binni <53377458+Binyamin-binni@users.noreply.github.com>
2020-11-22 15:14:14 +0500

    Update bxi.py

--- bxi.py
+++ bxi.py
@@ -1,2 +1,4 @@
+import os
+os.system("clear")
 print 3*("\n")
-print(" This tool is not available right now\n we are working it\n to grt update please subscribe our\n youtube channel TRICKPROOF\n click on the following link\n https://www.youtube.com/channel/UC5kptDqLxcVCzHgayaBuBPA")
+print(" This tool is not available right now\n we are working on it\n to grt update please subscribe our\n youtube channel TRICKPROOF\n click on the following link\n https://www.youtube.com/channel/UC5kptDqLxcVCzHgayaBuBPA")
